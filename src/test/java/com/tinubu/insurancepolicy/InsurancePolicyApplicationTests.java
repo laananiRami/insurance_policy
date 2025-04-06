694f0a9a96a6234bc4607a29ad48fe55b3d7c568
rami Laanani <laanai.rami@gmail.com>
2025-04-06 14:17:08 +0200

    Initial commit

---
+++ src/test/java/com/tinubu/insurancepolicy/InsurancePolicyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinubu.insurancepolicy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsurancePolicyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
